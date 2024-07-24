0c9e43c3a4542e925ff72f5a2b397b04dc632378
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-23 21:58:25 -0700

    add emojis

--- validate.py
+++ validate.py
@@ -9,13 +9,13 @@ with open(deck_file_path, "r") as deck_file:
 
 # validate the deck by checking if there are only 20 cards in the deck
 if len(deck) == 20:
-    print("Deck contains 20 cards")
+    print("✅ Deck contains 20 cards")
 else:
-    print("Deck is invalid. It should contain 20 cards")
+    print("❌ Deck is invalid. It should contain 20 cards")
 
 # validate the deck by checking that each card is unique
 deck_as_frozensets = {frozenset(card.items()) for card in deck}
 if len(deck) == len(deck_as_frozensets):
-    print("All cards are unique")
+    print("✅ All cards are unique")
 else:
-    print("Deck is invalid. All cards should be unique")
+    print("❌ Deck is invalid. All cards should be unique")
